99fab09c8dc2788d04ae24dfdb0b996327f56a52
Kishore <maruthikishore751@gmail.com>
2026-06-30 23:11:34 +0530

    Assertion

--- Playwrightautomation/StepDefinition/xpathLocator.ts
+++ Playwrightautomation/StepDefinition/xpathLocator.ts
@@ -1,10 +1,11 @@
 import { Given, Then, When } from "@cucumber/cucumber";
 import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { expect } from "playwright/test";
 
-let browser:Browser,context:BrowserContext,page:Page
+let browser: Browser, context: BrowserContext, page: Page
 Given('Launch the Browser', async function () {
-  browser = await chromium.launch( {headless: false,})
-  context = await browser.newContext({viewport:null})
+  browser = await chromium.launch({ headless: false, })
+  context = await browser.newContext({ viewport: null })
   page = await context.newPage()
 });
 
@@ -13,7 +14,9 @@ When('I Navigate to the Automation WebSite', async function () {
 });
 
 Then('I enter the Name using Xpath', async function () {
-  await page.locator("//input[@id='name']").fill("KISHORE M")
+  const name = page.locator("//input[@id='name']")
+  await name.fill("KISHORE M")  
+  await expect.soft(name).toHaveValue("KISHORE M")
 });
 
 Then('I enter the Email using Xpath', async function () {
@@ -39,7 +42,43 @@ Then('I Print the description of the page', async function () {
   console.log("Alert heading text:", alertText);
 });
 
-Then('I Locate the Element using Parent,preceding,following,following-sibling', async() =>{
-  await page.locator("//a[text()='Online Trainings']//preceding::a[text()='Udemy Courses']//preceding::a[text()='Home']//following::a[text()='Blog']").click()
+Then('I Locate the Element using Parent,preceding,following,following-sibling', async () => {
+  let Blog = await page.locator("//a[text()='Online Trainings']//preceding::a[text()='Udemy Courses']//preceding::a[text()='Home']//following::a[text()='Blog']")
+  //await Blog.click()
+})
+
+Then('Select Country Static DropDown', async () => {
+  let country = await page.locator("//select[@id='country']")
+  await country.scrollIntoViewIfNeeded()
+  await country.selectOption('China')
+})
+
+Then('Select Color Dynamic Dropdown', async () => {
+  let color = await page.locator("//select[@id='colors']")
+  await color.selectOption(['Red', 'Blue', 'Yellow'])
+})
+
+Then('Drag and Drop', async () => {
+  let drag = await page.locator("//div[@id='draggable']")
+  let drop = await page.locator("//div[@id='droppable']")
+  await drag.dragTo(drop)
 
 })
+
+Then('Take screenshot', async () => {
+  // Screenshot on Specific Field
+  let field= await page.locator("//input[@id='name']");
+  await field.screenshot({path:"./screenshot/Before.png"})
+  await field.fill('Edited')
+  await field.screenshot({path:"./screenshot/after.png"})
+
+  //screenshot on Visible page
+
+  await page.screenshot({path:"./screenshot/visiblepage.png"});
+
+  //Full page Screenshot
+
+  await page.screenshot({path:"./screenshot/fullpage.png",fullPage:true});
+  
+})
+
